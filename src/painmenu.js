e4f2466f3cb16a06df0be17d8bf2fd21ccea8afb
indra007s20 <76830399+indra007s20@users.noreply.github.com>
2021-01-23 11:10:23 -0300

    Add files via upload

---
+++ src/painmenu.js
@@ -0,0 +1,12 @@
+const painmenu = (prefix, pushname) => {
+    return `◪ *Comandos do pain*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.painmenu = painmenu

88f9c59131093e13119e57a6da36f0a969a522cd
indra007s20 <76830399+indra007s20@users.noreply.github.com>
2021-01-31 21:56:35 -0300

    Add files via upload

---
+++ src/toinmenu.js
@@ -0,0 +1,23 @@
+const toinmenu = (prefix, pushname) => {
+    return `◪ *Comandos do pain*
+╔════════════════════
+║╭──❉ *MENU DO TITIO PAIN* ❉── 
+║│➸${prefix}prefixo
+║│Sobre : esse muda o prefixo
+║│➸${prefix}block
+║│Sobre : esse da block
+║│➸${prefix}bc
+║│Sobre : esse da ban no numero usa naum
+║│➸${prefix}bcgc
+║│Sobre : esse da ban no numero de um geito diferente
+║│➸${prefix}clearall
+║│Sobre : esse limpa todas conversa do bot
+║╰───────────
+╠════════════════════
+║ TITIO PAIN O MAIS BRABOR
+╚════════════════════
+`
+
+}
+
+exports.toinmenu = toinmenu
